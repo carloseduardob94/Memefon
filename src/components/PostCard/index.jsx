0fa417bd436f6ca57174deca5887719405fd58f0
Eduardo <educonts08@gmail.com>
2022-10-29 13:30:51 -0300

    controles de video adicionados

--- src/components/PostCard/index.jsx
+++ src/components/PostCard/index.jsx
@@ -1,45 +1,46 @@
 import { Container } from './styles'
 
+
 import { HeaderCard } from '../HeaderCard'
 import { Footer } from '../Footer'
 
-
-export function PostCard({ post }){
-  const { 
+export function PostCard({ post }) {
+  const {
     title,
     videoUrl,
     imageUrl,
     category: { name },
     user: { username, photo },
     createdAt
-   } = post
+  } = post
 
   const renderContent = () => {
-    if(videoUrl){
-      return <video preload="auto" loop="loop" muted="muted" autoPlay="autoplay">
-        <source src={videoUrl} />
-      </video>
+    if (videoUrl) {
+      return (
+        <>
+          <video controls preload="auto" loop="loop" autoPlay="autoplay" muted >
+            <source src={videoUrl} />
+          </video>
+        </>
+      )
     }
-    if(imageUrl) return <img src={imageUrl} alt="" />
+    if (imageUrl) return <img src={imageUrl} alt="" />
 
     return null
   }
 
-  return(
+  return (
     <Container>
-      <HeaderCard 
-      photo={photo}
-      username={username}
-      date={createdAt}
-      category={name}
-    />
+      <HeaderCard
+        photo={photo}
+        username={username}
+        date={createdAt}
+        category={name}
+      />
 
       <main>
         <h2>{title}</h2>
-
-        <article>
-          {renderContent()}
-        </article>
+        <article>{renderContent()}</article>
       </main>
 
       <Footer />
--- src/components/PostCard/styles.js
+++ src/components/PostCard/styles.js
@@ -16,11 +16,39 @@ export const Container = styled.div`
     }
 
     article {
+      position: relative;
+
       video {
         width: 50rem;
         height: 50rem;
 
         border-radius: 15px;
+      }
+
+      #audio-button {
+        position: absolute;
+
+        border: none;
+        border-radius: 50%;
+
+        background-color: black;
+
+        padding: 0.8rem;
+
+        left: 0;
+        bottom: 0;
+
+        svg {
+          color: ${({ theme }) => theme.COLORS.WHITE};
+          font-size: 2.1rem;
+        }
+      }
+
+      .mute {
+        svg {
+          color: ${({ theme }) => theme.COLORS.WHITE};
+          font-size: 2.1rem;
+        }
       }
 
       img {
